6c851583cd43c547ddb09d24f6666114e11fd665
Yifan Zhang <evan.zhangyf@gmail.com>
2020-10-13 10:07:00 +0800

    24. 两两交换链表中的节点

---
+++ 题目记录/24. 两两交换链表中的节点.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head == None or head.next == None:
+            return head
+        tmp = head.next
+        head.next = self.swapPairs(tmp.next)
+        tmp.next = head
+        return tmp
