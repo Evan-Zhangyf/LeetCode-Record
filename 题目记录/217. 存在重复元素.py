823dfab088077ef8042c14c7b99c0107646a0250
Yifan Zhang <evan.zhangyf@gmail.com>
2020-12-13 11:22:16 +0800

    217. 存在重复元素

---
+++ 题目记录/217. 存在重复元素.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic = {}
+        for i in nums:
+            if not dic.get(i):
+                dic[i] = True
+            else:
+                return True
+        return False
