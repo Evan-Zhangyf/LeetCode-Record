7f11ae3dfe7cbbbdf952d2f14796e6f86b48017c
Yifan Zhang <evan.zhangyf@gmail.com>
2021-05-24 15:14:58 +0800

    modify 34. 在排序数组中查找元素的第一个和最后一个位置

--- 题目记录/34. 在排序数组中查找元素的第一个和最后一个位置.py
+++ 题目记录/34. 在排序数组中查找元素的第一个和最后一个位置.py
@@ -1,11 +1,28 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        ans = [-1, -1]
-        for i in range(len(nums)):
-            if nums[i] == target:
-                if ans[0] == -1:
-                    ans[0] = i
-                    ans[1] = i
-                else:
-                    ans[1] = i
+        # 特判
+        if nums == []:
+            return [-1, -1]
+        # 查找第一个大于等于target的位置
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m
+        if nums[l] != target:
+            return [-1, -1]
+        ans = [l]
+        # 查找最后一个等于target的位置
+        r = len(nums) - 1
+        from math import ceil
+        while l < r:
+            m = ceil((l + r) / 2)
+            if nums[m] > target:
+                r = m - 1
+            else:
+                l = m
+        ans.append(l)
         return ans
