6f1f874086d5017f757d78fe70b2b8a094355c18
Yifan Zhang <evan.zhangyf@gmail.com>
2021-12-17 12:18:51 +0800

    1518. 换酒问题

---
+++ 题目记录/1518. 换酒问题.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        empty = numBottles
+        ans = numBottles
+        while empty >= numExchange:
+            ans += empty // numExchange
+            empty = (empty // numExchange) + (empty % numExchange)
+        return ans
